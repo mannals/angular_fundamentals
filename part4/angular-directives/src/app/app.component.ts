87e45187726ad07b756d877444c124f2481070ba
mannals <annamalassu@gmail.com>
2025-02-26 10:15:48 +0200

    part 4 assignments 1, 2, 3

--- part4/angular-directives/src/app/app.component.ts
+++ part4/angular-directives/src/app/app.component.ts
@@ -2,13 +2,19 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ChangeColorDirective } from './change-color.directive';
 import { FontSizeDirective } from './font-size.directive';
+import { NgIf, NgOptimizedImage } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ChangeColorDirective, FontSizeDirective],
+  imports: [RouterOutlet, ChangeColorDirective, FontSizeDirective, NgIf, NgOptimizedImage],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'angular-directives';
+  title: string = 'angular-directives';
+  clicked: boolean = false;
+
+  onClick() {
+    this.clicked = true;
+  }
 }
